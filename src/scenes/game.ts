517d83527b4deddbd918be3139ee6aaa1338cbb9
Mark <mark@remarkablemark.org>
2024-01-16 21:54:24 -0500

    feat(sprites): add ghosty

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,5 +1,6 @@
 import { addCursorKeys } from '../events'
 import { addPlayer } from '../gameobjects'
+import { ghosty } from '../sprites'
 
 scene('game', () => {
   const player = addPlayer()
@@ -13,4 +14,10 @@ scene('game', () => {
   onClick(() => addKaboom(mousePos()))
 
   add([text('Press arrow keys', { width: width() / 2 }), pos(12, 12)])
+
+  for (let i = 0; i < 3; i++) {
+    const x = rand(0, width())
+    const y = rand(0, height())
+    add([ghosty, pos(x, y)])
+  }
 })
--- src/sprites/index.ts
+++ src/sprites/index.ts
@@ -1,1 +1,2 @@
 export * from './bean'
+export * from './ghosty'
